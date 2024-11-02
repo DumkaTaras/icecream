fa1657f332401b672a72ff129ab378bd4f8bd516
unknown <dumkataras@gmail.com>
2024-11-02 10:08:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector('.burger').onclick=function(){
+    document.querySelector('.menu').classList.toggle('active')
+  }
+  document.querySelector('.close-icon').onclick=function(){
+    document.querySelector('.close-icon').classList.toggle('close')
+  }
